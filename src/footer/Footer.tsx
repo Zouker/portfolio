dc2f7b92602779f571f0f2b6fbf703a9a13df0e6
Denis Shubin <denis.shubin@gmail.com>
2022-06-05 12:38:57 +0300

    feat: fill footer

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import styles from './Footer.module.css';
-import styleContainer from '../common/styles/Container.module.css';
+import styles from './Footer.module.scss';
+import styleContainer from '../common/styles/Container.module.scss';
+import github from '../assets/image/github.svg'
+import linkedIn from '../assets/image/linkedin.svg'
+import telegram from '../assets/image/telegram.svg'
+import email from '../assets/image/email.svg'
+
 
 export const Footer = () => {
     return (
@@ -8,10 +13,18 @@ export const Footer = () => {
             <div className={`${styleContainer.container} ${styles.footerContainer}`}>
                 <h2>Denis Shubin</h2>
                 <div className={styles.footerContacts}>
-                    <div className={styles.footerItem}></div>
-                    <div className={styles.footerItem}></div>
-                    <div className={styles.footerItem}></div>
-                    <div className={styles.footerItem}></div>
+                    <a href={'https://github.com/Zouker'} target={'_blank'}>
+                        <img src={github} className={styles.footerItem}/>
+                    </a>
+                    <a href={'https://www.linkedin.com/in/denis-shubin/'} target={'_blank'}>
+                        <img src={linkedIn} className={styles.footerItem}/>
+                    </a>
+                    <a href={'https://t.me/Zouker_BY'} target={'_blank'}>
+                        <img src={telegram} className={styles.footerItem}/>
+                    </a>
+                    <a href={'mailto:denis.shubin@gmail.com'}>
+                        <img src={email} className={styles.footerItem}/>
+                    </a>
                 </div>
                 <div className={styles.footerEnd}>© 2022 All Rights Reserved</div>
             </div>
