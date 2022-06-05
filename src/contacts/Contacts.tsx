0ba5dd8b25e26418daf820f95e89352baeef6c5c
Denis Shubin <denis.shubin@gmail.com>
2022-06-05 12:44:33 +0300

    fix: contacts block

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import styles from './Contacts.module.css'
-import styleContainer from '../common/styles/Container.module.css';
+import styles from './Contacts.module.scss'
+import styleContainer from '../common/styles/Container.module.scss';
+import {Title} from '../common/components/title/Title';
+import {Button} from '../common/components/button/Button';
 
 export const Contacts = () => {
     return (
         <div className={styles.contactsBlock}>
             <div className={`${styleContainer.container} ${styles.contactsContainer}`}>
-                <h2>Contacts</h2>
+                <Title text={'Contacts'}/>
                 <form className={styles.form}>
-                    <input className={styles.input}/>
-                    <input className={styles.input}/>
-                    <textarea className={styles.textarea}/>
+                    <input placeholder={'Name'} type={'text'} className={styles.input}/>
+                    <input placeholder={'E-mail'} type={'text'} className={styles.input}/>
+                    <textarea placeholder={'Your message'} className={styles.textarea}/>
                 </form>
-                <button className={styles.button}>Send</button>
+                <Button type={'submit'} buttonTitle={'Send message'}/>
             </div>
         </div>
     );
