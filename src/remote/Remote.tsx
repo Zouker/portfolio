23dafab6664a54bd9962284714925e1e6cbb2c2d
Denis Shubin <denis.shubin@gmail.com>
2022-07-07 13:28:11 +0300

    fix: fix 'hire me' button

--- src/remote/Remote.tsx
+++ src/remote/Remote.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Remote.module.scss';
 import {Title} from '../common/components/title/Title';
-import {Button} from '../common/components/button/Button';
+import button from '../common/components/button/Button.module.scss'
 
 const Flip = require('react-reveal/Flip')
 
@@ -13,7 +13,7 @@ export const Remote = () => {
                     <Title text={'Remote work and relocation are considered by me'}/>
                 </Flip>
                 <div className={styles.indent}>
-                    <Button buttonTitle={'Hire me'}/>
+                    <a href={'#contacts'} className={button.button}>Hire me</a>
                 </div>
             </div>
         </div>
