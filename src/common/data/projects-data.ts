8248638b2787983ed35e0ff657f70ca130d47f7a
Denis Shubin <denis.shubin@gmail.com>
2022-11-13 00:05:18 +0300

    feat: add project

--- src/common/data/projects-data.ts
+++ src/common/data/projects-data.ts
@@ -11,23 +11,24 @@ import websocketChat from '../../assets/image/chat-websocket.png'
 import rickAndMorty from '../../assets/image/rick-and-morty.png'
 import movieCatalog from '../../assets/image/movie-catalog.png'
 import weatherApp from '../../assets/image/weather-app.png'
+import homeworks from '../../assets/image/homeworks.png'
 
 export const projectsData = [
     {
         id: '1',
-        title: 'Todo List',
-        description: 'Technology stack: ReactJS, TypeScript, Redux Toolkit, RestAPI, Axios, Redux-Thunk, Material UI, Unit testing, Storybook',
-        backgroundImage: `url(${todoImage})`,
-        repoLink: 'https://github.com/Zouker/todolist-trello',
-        demoLink: 'https://zouker.github.io/todolist-trello/'
+        title: 'Homework Tasks',
+        description: 'Technology stack: ReactJS, TypeScript, Redux, Axios, CSS',
+        backgroundImage: `url(${homeworks})`,
+        repoLink: 'https://github.com/Zouker/homeworks',
+        demoLink: 'https://zouker.github.io/homeworks/'
     },
     {
         id: '2',
-        title: 'Social Network',
-        description: 'Technology stack: ReactJS, TypeScript, Redux, RestAPI, Axios, Redux-Thunk, React-Router-Dom, Redux-Form, Formik, Ant Design',
-        backgroundImage: `url(${socialImage})`,
-        repoLink: 'https://github.com/Zouker/social_network',
-        demoLink: 'https://zouker.github.io/social_network/'
+        title: 'Storybook of React components',
+        description: 'Technology stack: ReactJS, TypeScript, Storybook',
+        backgroundImage: `url(${storybook})`,
+        repoLink: 'https://github.com/Zouker/examples',
+        demoLink: 'https://zouker.github.io/examples/?path=/story/example-introduction--page'
     },
     {
         id: '3',
@@ -47,6 +48,22 @@ export const projectsData = [
     },
     {
         id: '5',
+        title: 'Todo List',
+        description: 'Technology stack: ReactJS, TypeScript, Redux Toolkit, RestAPI, Axios, Redux-Thunk, Material UI, Unit testing, Storybook',
+        backgroundImage: `url(${todoImage})`,
+        repoLink: 'https://github.com/Zouker/todolist-trello',
+        demoLink: 'https://zouker.github.io/todolist-trello/'
+    },
+    {
+        id: '6',
+        title: 'Social Network',
+        description: 'Technology stack: ReactJS, TypeScript, Redux, RestAPI, Axios, Redux-Thunk, React-Router-Dom, Redux-Form, Formik, Ant Design',
+        backgroundImage: `url(${socialImage})`,
+        repoLink: 'https://github.com/Zouker/social_network',
+        demoLink: 'https://zouker.github.io/social_network/'
+    },
+    {
+        id: '7',
         title: 'Cards',
         description: 'Technology stack: ReactJS, TypeScript, Redux, Redux-Thunk, React-Router-Dom, Axios, Formik, Material UI',
         backgroundImage: `url(${cards})`,
@@ -54,23 +71,16 @@ export const projectsData = [
         demoLink: 'https://zouker.github.io/cards/'
     },
     {
-        id: '6',
+        id: '8',
         title: 'Pizza-app',
         description: 'Technology stack: ReactJS, TypeScript, Redux Toolkit, Redux-Thunk, Axios',
         backgroundImage: `url(${pizza})`,
         repoLink: 'https://github.com/Zouker/pizza-app',
         demoLink: 'https://zouker.github.io/pizza-app/'
     },
+
     {
-        id: '7',
-        title: 'Storybook of React components',
-        description: 'Technology stack: ReactJS, TypeScript, Storybook',
-        backgroundImage: `url(${storybook})`,
-        repoLink: 'https://github.com/Zouker/examples',
-        demoLink: 'https://zouker.github.io/examples/?path=/story/example-introduction--page'
-    },
-    {
-        id: '8',
+        id: '9',
         title: 'Learning Cards',
         description: 'Technology stack: ReactJS, TypeScript, Redux, Redux-Thunk, React-Router-Dom, Axios, Formik, Material UI',
         backgroundImage: `url(${learningCards})`,
@@ -78,7 +88,7 @@ export const projectsData = [
         demoLink: 'https://zouker.github.io/learning-cards/'
     },
     {
-        id: '9',
+        id: '10',
         title: 'GitHub User Search',
         description: 'Technology stack: ReactJS, TypeScript, React-Router-Dom, Axios, CSS',
         backgroundImage: `url(${githubUserSearch})`,
@@ -86,7 +96,7 @@ export const projectsData = [
         demoLink: 'https://zouker.github.io/github-app/'
     },
     {
-        id: '10',
+        id: '11',
         title: 'Websocket Chat',
         description: 'Technology stack: ReactJS, TypeScript, Redux, Redux-Thunk, Socket.io',
         backgroundImage: `url(${websocketChat})`,
@@ -94,7 +104,7 @@ export const projectsData = [
         demoLink: 'https://zouker.github.io/chat-websocket/'
     },
     {
-        id: '11',
+        id: '12',
         title: 'Rick and Morty',
         description: 'Technology stack: ReactJS, TypeScript, Redux Toolkit, Redux-Thunk, React-Router-Dom, Axios, SASS',
         backgroundImage: `url(${rickAndMorty})`,
@@ -102,7 +112,7 @@ export const projectsData = [
         demoLink: 'https://zouker.github.io/rick-and-morty/'
     },
     {
-        id: '12',
+        id: '13',
         title: 'Movie Catalog',
         description: 'Technology stack: ReactJS, TypeScript, Redux, Redux-Thunk, Axios, CSS',
         backgroundImage: `url(${movieCatalog})`,
@@ -110,11 +120,11 @@ export const projectsData = [
         demoLink: 'https://zouker.github.io/movie-catalog/'
     },
     {
-        id: '13',
+        id: '14',
         title: 'Weather App',
         description: 'Technology stack: ReactJS, TypeScript, Axios, CSS',
         backgroundImage: `url(${weatherApp})`,
         repoLink: 'https://github.com/Zouker/weather-app',
         demoLink: 'https://zouker.github.io/weather-app/'
-    }
+    },
 ]
